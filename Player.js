84551f6c8a1c56a489db4c693ab023a6744a2f17
Daiara Dyba <daiaradyba@gmail.com>
2022-02-22 02:05:40 -0300

    2

---
+++ Player.js
@@ -0,0 +1,10 @@
+class Player{
+    constructor(){
+        this.body = createSprite(width/2, height -100,50,50);
+        this.body.addAnimation("playerAn",playerAn);
+        this.body.scale = 0.11;
+    }
+    jump(){
+        
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
 var bck_start;  
+var player, playerAn;
+
 function preload(){
-  bck_start = loadImage("BCK_ST.png")
+  bck_start = loadImage("./assets/BCK_ST.png");
+  playerAn = loadAnimation("./assets/Char_Pig_Idle_000.png","assets/Char_Pig_Idle_001.png","assets/Char_Pig_Idle_002.png",
+  "assets/Char_Pig_Idle_004.png","assets/Char_Pig_Idle_005.png","assets/Char_Pig_Idle_006.png","assets/Char_Pig_Idle_007.png",
+  "assets/Char_Pig_Idle_008.png","assets/Char_Pig_Idle_009.png")
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
- 
+  player = new Player();
 }
 
 function draw() {
-  //background(bck_start);  
-
-
+  background(bck_start);  
  
   drawSprites();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+function mouseClicked(){
+  console.log("x: "+mouseX," , y: "+mouseY)
+  player.jump();
 }

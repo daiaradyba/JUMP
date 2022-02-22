9b7364c6babb7b00e03074dfb39455ec960cd4a4
Daiara Dyba <daiaradyba@gmail.com>
2022-02-22 02:47:50 -0300

    3

--- Player.js
+++ Player.js
@@ -2,9 +2,19 @@ class Player{
     constructor(){
         this.body = createSprite(width/2, height -100,50,50);
         this.body.addAnimation("playerAn",playerAn);
+        this.body.addAnimation("playerJumpAn",playerJumpAn);
         this.body.scale = 0.11;
     }
-    jump(){
-        
+    gravity(x){
+        this.body.velocityY += x;
+        this.body.collide(invisible)
+    }
+    jumpStart(){
+    this.body.changeAnimation("playerJumpAn");
+    this.body.velocityY = -10
+    }
+    jumpEnd(){
+        this.body.changeAnimation("playerAn");
+      
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,28 +1,50 @@
 var bck_start;  
-var player, playerAn;
+var player, playerAn, playerJumpAn;
+var invisible;
 
 function preload(){
   bck_start = loadImage("./assets/BCK_ST.png");
   playerAn = loadAnimation("./assets/Char_Pig_Idle_000.png","assets/Char_Pig_Idle_001.png","assets/Char_Pig_Idle_002.png",
   "assets/Char_Pig_Idle_004.png","assets/Char_Pig_Idle_005.png","assets/Char_Pig_Idle_006.png","assets/Char_Pig_Idle_007.png",
   "assets/Char_Pig_Idle_008.png","assets/Char_Pig_Idle_009.png")
-}
+  playerAn.frameDelay = 2
+
+  playerJumpAn = loadAnimation("./assets/Char_Pig_Flyght_000.png","./assets/Char_Pig_Flyght_001.png","./assets/Char_Pig_Flyght_002.png",
+  "./assets/Char_Pig_Flyght_003.png","./assets/Char_Pig_Flyght_004.png","./assets/Char_Pig_Flyght_005.png",
+  "./assets/Char_Pig_Flyght_006.png","./assets/Char_Pig_Flyght_007.png","./assets/Char_Pig_Flyght_008.png","./assets/Char_Pig_Flyght_009.png");
+  playerJumpAn.frameDelay = 1
+
+} 
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   player = new Player();
+  invisible = createSprite(windowWidth/2,windowHeight-50,windowWidth,5)
+  invisible.visible = false
 }
 
 function draw() {
   background(bck_start);  
- 
+
+  if(mouseIsPressed){
+    console.log("jump")
+    player.jumpStart();
+  }
+  if(!mouseIsPressed){
+    console.log("re")
+    player.jumpEnd();
+  }
+
+  
+  player.gravity(2);
   drawSprites();
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-function mouseClicked(){
-  console.log("x: "+mouseX," , y: "+mouseY)
-  player.jump();
+function mouseReleased(){
+  
 }
+

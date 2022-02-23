91b02891597ee34d16365684bcb14d29735e60d5
Daiara Dyba <daiaradyba@gmail.com>
2022-02-23 00:00:29 -0300

    16

--- Player.js
+++ Player.js
@@ -14,7 +14,7 @@ class Player{
     }
     jumpStart(){
     this.body.changeAnimation("playerJumpAn");
-    this.body.velocityY = -5
+    this.body.velocityY = -10
     var index = touches.length -1 
     console.log(touches)
     if(this.body.x<touches[index].x){

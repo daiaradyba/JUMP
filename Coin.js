2c7400f4559e9565b6a44f7497d04f8bb2f90d9c
Daiara Dyba <daiaradyba@gmail.com>
2022-03-14 17:06:35 -0300

    COIN SPAWN AND RESTART

--- Coin.js
+++ Coin.js
@@ -1,10 +1,11 @@
-class Steak{
+
+class Coin{
     constructor(x,y,velocityX,velocityY){
         this.body = createSprite(x,y);
-        this.body.addAnimation("steakAn",steakAn);
+        this.body.addAnimation("coinAn",coinAn);
         this.body.velocityX = velocityX;
         this.body.velocityY = velocityY;
-        this.body.scale = 0.1;
+        this.body.scale = 0.8;
         this.body.lifetime = 500;
     }
     
--- sketch.js
+++ sketch.js
@@ -13,16 +13,17 @@ var start = false;
 var limiteinf;
 var gameState = 0;
 var startSprite
-var startBtn;
+var startBtn,restartSprite, restartAn;
 var pont = 0; 
 var pontTag;
 var texto;
 var fonte;
-var coinStAn,coinSt;
+var coinStAn,coinSt,coin,coinGroup;
 var coinCollect = 0;
 var qntSteak = 3;
-var nApoio = 0, sorteiaApoio
-
+var nApoio = 0, sorteiaApoio,nBife=0,sorteiaBife;
+var teste;
+var vel_Y = 5;
 
 function preload(){
   bck_start = loadImage("./assets/BCK.png");
@@ -53,7 +54,12 @@ function preload(){
 
   fonte = loadFont("assets/Font/LuckiestGuy-Regular.otf")
 
-  coinStAn = loadImage("./assets/COIN/1.png")
+  coinStAn = loadImage("./assets/COIN/1.png");
+
+  coinAn = loadAnimation("./assets/COIN/1.png","./assets/COIN/2.png","./assets/COIN/3.png","./assets/COIN/4.png",
+  "./assets/COIN/5.png","./assets/COIN/6.png");
+
+  restartAn = loadAnimation("./assets/GUI/PlayAgainBtn.png");
 } 
 
 function setup() {
@@ -75,14 +81,23 @@ function setup() {
   gApoio = new Group();
   
   gyro = new Gyroscope();
+  
+  coinGroup = new Group();
 
   
  
-  createSteakStart();
+
 
   startSprite = createSprite(windowWidth/2,windowHeight/2);
   startSprite.addAnimation("start",startBtn)
   startSprite.scale = 0.3
+
+  restartSprite = createSprite(windowWidth/2,windowHeight/2)
+  restartSprite.addAnimation("restart",restartAn);
+  restartSprite.scale = 0.3;
+  restartSprite.visible = false;
+
+  createSteakStart();
 
   coinSt = createSprite((windowWidth*3/4-15),120);
   coinSt.addAnimation("coinSt",coinStAn);
@@ -90,24 +105,30 @@ function setup() {
 
   sorteiaApoio=Math.round(random(1,3));
   nApoio = 0;
+  nBife = 0;
+  sorteiaBife = Math.round(random(1,3));
+
+  teste = new Coin(100,100,0,0)
  
  
 }
 
 function draw() {
   background(bck_start);  
-  console.log(player.gas);
+  //console.log(player.gas);
   
 
 
     if(gameState===0){
-      console.log("0")
+      //console.log("0")
       // start.depth = player.body.depth+5
+      
       if(touches.length>0){
         if(touches[touches.length-1].x<startSprite.x+startSprite.width/2&&touches[touches.length-1].x>startSprite.x-startSprite.width/2&&touches[touches.length-1].y<startSprite.y+startSprite.height/2&&touches[touches.length-1].y>startSprite.y-startSprite.height/2){
           console.log("CLICOU");
           startSprite.destroy();
           touches = [];
+          
           gameState = 1;
         }
       }
@@ -138,14 +159,13 @@ function draw() {
      }  
  
     if(player.body.isTouching(invisible)&&pont>15){
-    gameState=2;
+      coinGroup.destroyEach();
+      steakGroup.destroyEach();
+      gApoio.destroyEach();
+      gameState=2;
   }
 
-  if(gameState===2){
-    player.body.changeAnimation("playerdead")
- 
-    bck.velocityY = 0  
-  }
+  
   
  //console.log("bck.y: "+bck.y+" player.y: "+ player.y)
  
@@ -154,6 +174,39 @@ function draw() {
   player.gravity(2);
   
   
+  }
+
+  if(gameState===2){
+    console.log("2")
+    player.body.changeAnimation("playerdead")
+ 
+  
+
+    bck.velocityY = 0  
+    start = false; 
+    restartSprite.visible = true;
+    restartSprite.debug = true;
+
+    if(touches.length>0){
+
+      if(touches[touches.length-1].x<restartSprite.x+restartSprite.width/2&&touches[touches.length-1].x>restartSprite.x-restartSprite.width/2&&touches[touches.length-1].y<restartSprite.y+restartSprite.height/2&&touches[touches.length-1].y>restartSprite.y-restartSprite.height/2){
+        console.log("RESTART");
+        restartSprite.visible = false;
+        touches = [];
+        pont = 0;
+        coinCollect = 0;
+        player.gas = 100;
+        //startSprite = createSprite(windowWidth/2,windowHeight/2);
+       // startSprite.addAnimation("start",startBtn)
+        //startSprite.scale = 0.3
+        createSteakStart();
+        startfundo = false;
+        gameState = 1;
+        console.log(gameState)
+      }
+    }
+
+    console.log(gameState)
   }
   //END GAME STATE 1
 
@@ -172,7 +225,7 @@ push()
   textSize(30)
   text(coinCollect, windowWidth*3/4+10,coinSt.y+coinSt.width/4-2)
   pop();
-  console.log("sorteia: "+sorteiaApoio + " NAPOIO: "+ nApoio)
+  
 }
 
 //END DRAW
@@ -210,7 +263,7 @@ function movCamera(){
     startfundo = true;
   }
   if(startfundo){
-    bck.velocityY =5;
+    bck.velocityY =vel_Y;
     gerarApoio();
     steakGroup.setVelocityYEach(+5);
 
@@ -225,9 +278,9 @@ function movCamera(){
 }
 
 function gerarApoio(){
-  if(frameCount%90===0){
+  if(frameCount%78===0){
     var apoio = createSprite(random(100,windowWidth-100), 0,350,50)
-    apoio.velocityY = 5;
+    apoio.velocityY = vel_Y;
     apoio.lifetime = 400;
     apoio.debug = true;
     apoio.addImage(imgApoio);
@@ -243,16 +296,30 @@ function gerarApoio(){
     player.depth = apoio.depth +1;
     apoio.depth = player.depth -1;
 
+
+    console.log("sorteia: "+sorteiaApoio + " NAPOIO: "+ nApoio + " NBIFE:  "+ nBife + " SORTEIACOIN: " + sorteiaBife)  
+
     if(nApoio===0){
       sorteiaApoio = Math.round(random(1,3))
      
    
     }
-
+    if(nBife ===0){
+    sorteiaBife = Math.round(random(1,6));  
+    console.log("sorteou bife : " + sorteiaBife)
+     
+    }
     if(nApoio===sorteiaApoio){
       createSteak();
+      nBife +=1;
       nApoio = 0;
     }
+    if(nBife===sorteiaBife){
+      createCoin();
+      nBife = 0;
+    }
+
+
     nApoio +=1;
     gApoio.add(apoio);
     
@@ -271,18 +338,28 @@ function createSteakStart(){
 }
 }
 
+function createCoin(){
+
+ 
+    coin = new Coin(random(windowWidth-windowWidth*3/4,windowWidth-windowWidth/4),-10,0,vel_Y);
+    coinGroup.add(coin.body);
+  
+
+}
+
 function createSteak(){
 
  
-    steak = new Steak(random(windowWidth-windowWidth*3/4,windowWidth-windowWidth/4),-10,0,10);
-    steakGroup.add(steak.body);
-  
+  steak = new Steak(random(windowWidth-windowWidth*3/4,windowWidth-windowWidth/4),-10,0,vel_Y);
+  steakGroup.add(steak.body);
+
 
 }
 
 async function allthetime(){
   
   player.body.bounceOff(steakGroup,removeSteak)
+  player.body.bounceOff(coinGroup,removeCoin)
 }
 function removeSteak(splayer,steak){
   
@@ -290,6 +367,17 @@ function removeSteak(splayer,steak){
   console.log("COLIDI")
   steakGroup.remove(steak);
   steak.remove();
+  
+
+}
+
+function removeCoin(splayer,coin){
+  
+
+
+  coinGroup.remove(coin);
+  coinCollect +=1;
+  coin.remove();
   
 
 }
